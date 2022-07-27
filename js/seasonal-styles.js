102a4caaf14fdce80e06f9da23e64d5a925f98ec
yagirldolor <deusebio@sea.yearup.org>
2022-07-27 12:35:39 -0700

    update

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,17 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+
+        $("html").css("background-color", "pink");
+
+        //left what is the attribute, right what to change the attribute to
+        $("#wear").attr("src", "images/spring-wear.jpg");
+
+        $("#logo").attr("src", "images/spring.gif");
+
+        $("header h3").text("Jump into Spring wear!");
+
+
     });
 });
